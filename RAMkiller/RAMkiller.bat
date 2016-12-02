e70dd2a9b9b3b5f2a899f99dd7199b61b45537cf
Tien Vu <timothyvu2002@gmail.com>
2016-12-02 13:07:58 -0500

    Update RAMkiller.bat

--- RAMkiller/RAMkiller.bat
+++ RAMkiller/RAMkiller.bat
@@ -6,8 +6,18 @@ color 0a
 
 -=These words are not a comment, they are here to prevent accidental running (it's an issue)
 -=To make the program work faster, just copy-paste "start RAMkiller.bat" where it says to farther down
--=You'll want to delete the "pause" line and lines starting with a -= to run the code
+-=You'll want to delete:
+-=-=The echo command below
+-=-=The pause command below
+-=-=The "goto 2" command below
+-=-=Anything with "-=" or "-=-=" in front of it
+
+-=You are now ready to go!
+
+echo You forgot to delete some lines of code! (including this one!)
 pause
+
+goto 2
 
 type RAMK.txt
 start RAMkiller.bat
@@ -22,4 +32,5 @@ start RAMkiller.bat
 
 goto 1
 
+:2
 stop
